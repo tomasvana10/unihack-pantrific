4c673f9a803e0bba186d10dd74bd2e20efc8face
Manjunath Sharma <mmaniyoormadham@gmail.com>
2026-03-15 12:54:03 +1100

    updat env checking

--- apps/api/drizzle.config.ts
+++ apps/api/drizzle.config.ts
@@ -2,7 +2,11 @@ import { readFromEnv } from "@pantrific/shared/utils";
 import dotenv from "dotenv";
 import { defineConfig } from "drizzle-kit";
 
-dotenv.config({ path: ".env.development" });
+const envFile =
+  process.env.NODE_ENV === "production"
+    ? ".env.production"
+    : ".env.development";
+dotenv.config({ path: envFile });
 
 export default defineConfig({
   schema: "./src/db/schema",
